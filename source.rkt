0f7701ca9ae62b7c330eeaa8f0299a141f4cf362
Sean Fitzpatrick <seanfitzpatrick@ymail.com>
2017-03-28 12:46:05 +0000

    Define listOfNum, ops and random number

---
+++ source.rkt
@@ -0,0 +1,11 @@
+#lang racket
+;Author : Sean Fitzpatrick
+
+;Define list of numbers to be used for generating six random numbers
+(define ListOfNum (list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 25 50 75 100))
+
+;Define list of operators
+(define ops (list + - / *))
+
+;Define random number between 101 and 999
+(define rand (random 101 1000))
