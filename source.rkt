a2c2c4a0fd52800292fc0b71b0502a2d9dd6129d
Sean Fitzpatrick <seanfitzpatrick@ymail.com>
2017-04-02 20:36:13 +0100

    procedure list-ref

--- source.rkt
+++ source.rkt
@@ -11,7 +11,15 @@
 (define rand (random 101 1000))car ops
 
 ;Returns nth element from the list
-(define a(list-ref (list 'a 'b 'c 'd 'e 'f) 4))
+(define a(list-ref (list 1 2 3 4 5 6) 4))
+
+; Define procedure with two arguments, list and pos
+(define (my-list-ref ls n)
+  ; If pos is zero return first element in the list with car
+  (if (zero? n)
+      (car ls)
+      ;Else return the rest of the list and get elemnt at pos n-1
+      (my-list-ref (cdr ls) (- n 1))))
 
 
 
